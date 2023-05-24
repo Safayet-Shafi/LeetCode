9ba193cc1e092e3d50807a7b552f8241ce030a14
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2023-05-24 23:51:38 +0600

    tosum

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,14 @@ public class Main {
         RemoveDuplicateElement obj2=new RemoveDuplicateElement();
 
         ToSum obj3=new ToSum();
-        int[] target1 =obj3.twoSum(new int[]{2,4,5},6);
+        int[] target1 =obj3.twoSum(new int[]{2,7,11,15},9);
+        System.out.println(target1.length);
 
         for(int i=0;i<target1.length;i++){
-            if(target1[i]!=0){
+
                 System.out.print(target1[i]+" ");
-            }
+
+
         }
 
         int arr[] ={3,2,2,3};
--- src/ToSum.java
+++ src/ToSum.java
@@ -1,17 +1,16 @@
 public class ToSum {
     public int[] twoSum(int[] nums, int target) {
-        int [] temp = new int[2];
-      for(int i=0;i<nums.length-1;i++){
-          for(int j=i+1;j<nums.length-1;j++){
-              if(nums[i]+nums[j]==target){
-                  temp[i]=nums[i];
-                  temp[j]=nums[j];
-
-
-              }
-              return  temp;
-          }
-      }
-      return temp;
+        int[] ans = new int[2];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] + nums[i]== target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
     }
 }
